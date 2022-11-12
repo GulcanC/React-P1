0087f13718bb28486ae93c6fd7584b6ba542947e
GulcanC <gulcancoskun.a@gmail.com>
2022-11-12 13:42:59 +0100

    create file style.js

--- GC-project-react-vite/src/App.jsx
+++ GC-project-react-vite/src/App.jsx
@@ -2,9 +2,11 @@
 
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className=""></div>
+  </div>
+);
 
 export default App;
 
